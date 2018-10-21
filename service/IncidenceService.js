aa62394320b4497c883e89fc3fb1e81bf0691ed7
Arnel Pällo <arnel.pallo@taxify.eu>
2018-10-21 11:16:23 +0300

    s

--- service/IncidenceService.js
+++ service/IncidenceService.js
@@ -9,11 +9,13 @@ class IncidenceService {
         this.db.query(`select * from orbits where utc between DATE_SUB('${datetime}', INTERVAL 1 MINUTE) and '${datetime}' `, (err, res) => {
             let pointy;
             for (const row of res) {
+                console.log(row);
                 if (Distances.getDistance(lat, lng, row.lat, row.lng) < 1000) {
-                    pointy = {lat: row.lat, lng: row.lng, bearing: row.bearing};
+                    pointy = {lat: row.lat, lng: row.lng, bearing: row.direction};
                     break;
                 }
             }
+            console.log(pointy);
             // const lat1 = 59.17029;
             // const lng1 = 23.96118;
             // const lat2 = 58.13012;
@@ -21,7 +23,9 @@ class IncidenceService {
             // const latX = 58.68836;
             // const lngX = 22.90649;
 
-
+            if (!pointy) {
+                return void callback("no match");
+            }
             return void callback(Distances.getNearestPoint(
                 pointy.lat, pointy.lng, pointy.bearing, lat, lng
             ));
